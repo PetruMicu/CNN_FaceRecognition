0928c1228e00d3c3a882f53f6974d72b6d8d448d
Petru <micu.petru2899@gmail.com>
2024-01-14 13:09:21 +0200

    updated dataset and code now returns first 3 neighbours

--- CNN_FaceRecognition.py
+++ CNN_FaceRecognition.py
@@ -5,6 +5,9 @@ from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_i
 from tensorflow.keras.preprocessing import image
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
+# import matplotlib.pyplot as plt
+# import matplotlib.image as mpimg
+# from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 # Function to preprocess image for MobileNetV2
 def preprocess_image(img):
@@ -16,6 +19,7 @@ def preprocess_image(img):
 # Load MobileNetV2 model without the top classification layer
 base_model = MobileNetV2(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
 
+
 # Function to extract features using MobileNetV2
 def extract_features(face):
     face_array = preprocess_image(face)
@@ -26,11 +30,11 @@ def extract_features(face):
 def get_person_name(predicted_label):
     # Define a mapping between labels and person names
     label_to_person = {
-        0: "Person1",
-        1: "Person2",
-        2: "Person3",
-        3: "Person4",
-        4: "Person5"
+        0: "Mark Rutte (person1)",
+        1: "Sigrid Kaag (person2)",
+        2: "Carola Schouten (person3)",
+        3: "Hugo de Jonge (person4)",
+        4: "Robbert Dijkgraaf (person5)"
     }
 
     # Return the corresponding person name for the predicted label
@@ -84,7 +88,7 @@ embeddings = np.array(embeddings)
 labels = np.array(labels)
 
 # Train a simple k-NN classifier
-knn_classifier = KNeighborsClassifier(n_neighbors=5)
+knn_classifier = KNeighborsClassifier(n_neighbors=3)
 knn_classifier.fit(embeddings, labels)
 
 # Function to perform face recognition
@@ -96,7 +100,8 @@ def recognize_face(test_img):
         # Extract features from the cropped face
         test_embedding = extract_features(face).flatten()  # Flatten the features
         predicted_label = knn_classifier.predict(test_embedding.reshape(1, -1))[0]
-        return predicted_label
+        k_neighbours = knn_classifier.kneighbors(test_embedding.reshape(1, -1), return_distance=False)
+        return predicted_label, k_neighbours
     else:
         return None
 
@@ -111,13 +116,43 @@ for label, personality in enumerate(sorted(os.listdir(dataset_path))):
         # Read the image using OpenCV
         test_img = cv2.imread(test_image_path)
         # Use the recognize_face function to get the predicted label
-        predicted_label = recognize_face(test_img)
+        predicted_label, k_neighbours = recognize_face(test_img)
 
         if predicted_label is not None:
             person_name = get_person_name(predicted_label)
             print("Input Image:", test_image_path)
             print("Predicted Person:", person_name)
             print()
+            print("Closest images are:")
+            img_path = []
+            img_titles = []
+            for neighbor in k_neighbours[0]:
+                person = (neighbor // 7) + 1
+                img = (neighbor % 7) + 1
+                img_path.append(f"./dataset/person{person}/reference/image{img}.jpg")
+                img_titles.append(f"person{person}/reference/image{img}.jpg")
+                print(f"./dataset/person{person}/reference/image{img}.jpg")
+            print()
+
+            # num_images = len(img_path) + 1  # Add 1 for the test image
+            # fig, axes = plt.subplots(1, num_images, figsize=(15, 5))  # Adjust the figsize as needed
+            # plt.subplots_adjust(wspace=1)  # You can adjust the spacing as needed
+
+            # # Display the test image
+            # test_img = mpimg.imread(test_image_path)
+            # axes[0].imshow(test_img)
+            # axes[0].set_title("Test Image")
+            # axes[0].axis('off')
+
+            # # Loop through the image paths and display them in subplots
+            # for i, path in enumerate(img_path):
+            #     img = mpimg.imread(path)
+            #     axes[i + 1].imshow(img)
+            #     axes[i + 1].set_title(img_titles[i])
+            #     axes[i + 1].axis('off')
+
+            # plt.show()
+
 
             true_labels.append(label)
             predicted_labels.append(predicted_label)
